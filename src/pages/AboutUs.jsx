9356bf1784aff1c6f10a311c59e59dcdf51dd515
Gabriel Ford <gabrielford@Gabriels-iMac.local>
2022-02-07 09:50:19 -0500

    currently working on the About Us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,8 +3,34 @@ import React from 'react';
 
 let AboutUs = () => {
     return (
-        <div style={{ "display": "flex" }}>
-            <h1>Hey 😄 thanks for being curious enough to check this out! It is awesome to me that I was able to make your flight search experience better!</h1>
+        <div style={{
+            "display": "flex",
+            "alignItems": "center",
+            "justifyContent": "center"
+        }}>
+            <div style={{
+                "marginTop": "5%",
+                "display": "flex",
+                "flexDirection": 'column',
+                "width": "70%",
+                "alignItems": "center",
+                "justifyContent": "center"
+            }}>
+                <div >
+                    <h1 className='text-5xl font-medium'> 😄 Hey 👋 </h1>
+                </div>
+                <div className='mt-10'>
+                    <p className='text-md text-center'>Thanks for being curious enough to check this out! It is awesome to me that I was able to make your flight search experience better!</p>
+                </div>
+                <div className='mt-5'>
+                    <p className='text-md text-center'>My name is Gabriel Ford, I'm a software engineer and lover of learning. If you have any cool project ideas or if you have any suggestions on the website please contact me at gabe@fordltc.net. Thanks again!</p>
+                </div>
+                <div className='mt-5'>
+                    <p className='text-center'>Have a great day!<br />
+                        -Gabriel Ford
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
